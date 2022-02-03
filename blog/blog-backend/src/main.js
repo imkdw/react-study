a9c1d53882f041f2cdbb25090b43ebdad290abc8
Dongwoo-Kim <dongeu47@gmail.com>
2022-02-03 19:52:09 +0900

    220203

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -14,6 +14,7 @@ mongoose
   .catch(e => console.error(e));
 
 import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware';
 
 const app = new Koa();
 const router = new Router();
@@ -21,6 +22,7 @@ const router = new Router();
 router.use('/api', api.routes());
 
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 app.use(router.routes()).use(router.allowedMethods());
 

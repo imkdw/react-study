23b5209c330851027f7a385ab7b5305465ff1d48
Dongwoo-Kim <dongeu47@gmail.com>
2022-02-06 12:51:11 +0900

    220206

---
+++ React_Practice/nomadcoders-hooks/src/App.js
@@ -0,0 +1,11 @@
+// import { useTabs } from "./hooks/useTabs";
+// import { useInputs } from "./hooks/useInput";
+
+const App = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default App;

0b6d3eed5068150977e58c348ae4a6f9d2dfca50
Dongwoo-Kim <dongeu47@gmail.com>
2022-02-14 20:18:26 +0900

    220214

--- React_Practice/learn-redux-middleware/src/App.js
+++ React_Practice/learn-redux-middleware/src/App.js
@@ -1,7 +1,11 @@
-import CounterContainer from "./containers/CounterContainer";
+import SampleContainer from "./containers/SampleContainer";
 
 const App = () => {
-  return <CounterContainer />;
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  );
 };
 
 export default App;
--- React_Practice/learn-redux-middleware/src/modules/sample.js
+++ React_Practice/learn-redux-middleware/src/modules/sample.js
@@ -1,6 +1,6 @@
 import { handleActions } from "redux-actions";
 import * as api from "../lib/api";
-import produce from "immer";
+import createRequestThunk from "../lib/createRequestThunk";
 
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
@@ -11,41 +11,8 @@ const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
 const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
 // thunk 함수 생성, 함수 내부에서는 시작, 성공, 실패에 따른 다른 액션을 디스패치
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST });
-  try {
-    const response = await api.getPost(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e;
-  }
-};
-
-export const getUsers = (id) => async (dispatch) => {
-  dispatch({ type: GET_USERS });
-  try {
-    const response = await api.getUsers(id);
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e;
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 const initialState = {
   loading: {
@@ -56,20 +23,54 @@ const initialState = {
   users: null,
 };
 
-const sample = handleActions({
-  [GET_POST]: (state) => ({
-    ...state,
-    loading: {
-      ...state.loading,
-      GET_POST: true, // 요청 시작
-    },
-  }),
-  [GET_POST_SUCCESS]: (state, { paylood: post }) => ({
-    ...state,
-    loading: {
-      ...state.loading,
-      GET_POST: false, // 요청 완료
-    },
-    post: post,
-  }),
-});
+const sample = handleActions(
+  {
+    [GET_POST]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_POST: true, // 요청 시작
+      },
+    }),
+    [GET_POST_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_POST: false, // 요청 완료
+      },
+      post: action.payload,
+    }),
+    [GET_POST_FAILURE]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_POST: false, // 요청 완료
+      },
+    }),
+    [GET_USERS]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USERS: true, // 요청 시작
+      },
+    }),
+    [GET_USERS_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USERS: false, // 요청 완료
+      },
+      users: action.payload,
+    }),
+    [GET_USERS_FAILURE]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USERS: false, // 완료
+      },
+    }),
+  },
+  initialState
+);
+
+export default sample;

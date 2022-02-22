9ee7771d130b6e1ead5bfc680d45110b5a4f0a40
Dongwoo-Kim <dongeu47@gmail.com>
2022-02-22 21:06:42 +0900

    220222

--- React_Project/bitcoin-info/src/Components/Coins.tsx
+++ React_Project/bitcoin-info/src/Components/Coins.tsx
@@ -9,17 +9,30 @@ interface Coin {
   units_traded: string;
 }
 
+const coinName: any = {
+  BTC: "비트코인/BTC",
+};
+
+// const fakeData = {
+//   timestamp: 0000000000000,
+//   payment_currency: 'KRW',
+//   BTC :{},
+//   ETH: {},
+// }
+
 const Coins = () => {
   const [coins, setCoins] = useState<any[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<any>(null);
+
+  const CORS_PROXY_URL = "https://imkdw-cors.herokuapp.com/";
 
   useEffect(() => {
     const getCoins = async () => {
       try {
         setLoading(true);
         const response = await axios.get(
-          "https://api.bithumb.com/public/orderbook/ALL_KRW"
+          CORS_PROXY_URL + "https://api.bithumb.com/public/orderbook/ALL_KRW"
         );
         setCoins(response.data.data);
       } catch (e) {
@@ -29,32 +42,28 @@ const Coins = () => {
       setLoading(false);
     };
 
-    getCoins();
+    setInterval(getCoins, 1000);
   }, []);
 
-  if (loading) return <h1>로딩중입니다..</h1>;
   if (!coins) return <h1>코인이 없습니다.</h1>;
   if (error) return <h1>에러발생!!!</h1>;
 
   const krwCoins: any = [];
   for (const coin in coins) {
-    const infoObj = Object.assign({ name: coin }, coins[coin]);
-    krwCoins.push(infoObj);
+    krwCoins.push(Object.assign({ name: coin, ...coins[coin] }));
   }
-
-  console.log(krwCoins);
 
   return (
     <ul>
-      {krwCoins.slice(0, krwCoins.length - 1).map((coin: any) => {
-        const { name, price } = coin;
+      {krwCoins.slice(2).map((coin: any) => {
+        const { name, asks } = coin;
+        const price = Number(asks[0].price);
         return (
           <li key={name}>
+            <div>{name in coinName ? coinName[name] : name}</div>
             <div>
-              <b>{name}</b>
+              현재가격 : {price % 1 ? price : price.toLocaleString("ko-kr")}원
             </div>
-            <div>현재가격 : {price}원</div>
-            <hr />
           </li>
         );
       })}

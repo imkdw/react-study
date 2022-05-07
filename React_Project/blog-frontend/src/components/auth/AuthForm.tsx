019a03bc258dcc4567bd517d0dda5e439966cd98
Dongwoo-Kim <dongeu47@gmail.com>
2022-05-07 17:15:51 +0900

    220507
    752P / 리덕스로 상태관리하기

---
+++ React_Project/blog-frontend/src/components/auth/AuthForm.tsx
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import palette from '../../lib/styles/palette';
+import Button from '../common/Button';
+
+/** 새로운 css 속성을 위해 새로운 버튼 정의 */
+const ButtonWidthMarginTop = styled(Button)`
+  margin-top: 1rem; ;
+`;
+
+/**
+ * 회원가입 or 로그인 폼 컴포넌트
+ */
+
+const AuthFormBlock = styled.div`
+  margin: 0;
+  color: ${palette.gray[8]};
+  margin-bottom: 1rem;
+`;
+
+const StyledInput = styled.input`
+  font-size: 1rem;
+  border: none;
+  border-bottom: 1px solid ${palette.gray[5]};
+  padding-bottom: 0.5rem;
+  outline: none;
+  width: 100%;
+
+  &:focus {
+    color: $oc-teal-7;
+    border-bottom: 1px solid ${palette.gray[7]};
+  }
+
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+/** 폼 하단 로그인 or 회원가입 링크 */
+const Footer = styled.div`
+  margin-top: 2rem;
+  text-align: right;
+  a {
+    color: ${palette.gray[6]};
+    text-decoration: underline;
+    &:hover {
+      color: ${palette.gray[9]};
+    }
+  }
+`;
+
+interface IAuthFormProps {
+  type: 'login' | 'register';
+}
+
+const textMap = {
+  login: '로그인',
+  register: '회원가입',
+};
+
+const AuthForm = ({ type }: IAuthFormProps) => {
+  const text: string = textMap[type];
+
+  return (
+    <AuthFormBlock>
+      <h3>{text}</h3>
+      <form>
+        <StyledInput
+          autoComplete="username"
+          name="username"
+          placeholder="아이디"
+        />
+        <StyledInput
+          autoCapitalize="new-password"
+          name="password"
+          placeholder="비밀번호"
+          type="password"
+        />
+        {type === 'register' && (
+          <StyledInput
+            autoComplete="new-password"
+            name="passwordConfirm"
+            placeholder="비밀번호 확인"
+            type="password"
+          />
+        )}
+        <ButtonWidthMarginTop fullWidth cyan>
+          {text}
+        </ButtonWidthMarginTop>
+      </form>
+      <Footer>
+        {type === 'login' ? (
+          <Link to="/register">회원가입</Link>
+        ) : (
+          <Link to="/login">로그인</Link>
+        )}
+      </Footer>
+    </AuthFormBlock>
+  );
+};
+
+export default AuthForm;
---
+++ React_Project/blog-frontend/src/modules/auth.tsx
@@ -0,0 +1,60 @@
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+/** changeField 액션 생성 */
+interface IChangeField {
+  form: string;
+  key: string;
+  value: string;
+}
+
+export const initializeForm = createAction(
+  INITIALIZE_FORM,
+  (form: string) => form,
+);
+
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }: IChangeField): any => ({
+    form, // register, login 구분
+    key, // input에서 사용할 username, password, passwordConfirm
+    value, // 실제로 입력되어서 변경할 값
+  }),
+);
+
+/**
+ * 초기 폼을 위한 액션
+ * login or register가 이에 해당
+ */
+
+
+
+
+const initialState = {
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  login: {
+    username: '',
+    password: '',
+  },
+};
+
+const auth = handleActions({
+  [CHANGE_FIELD]: (state, {payload : {form, key, value}}) => {
+    produce(state, draft => {
+      draft[form][key] = value;
+    })
+  },
+  {INITIALIZE_FORM}: (state, payload: {form}) => ({
+    ...state,
+    [form]: initialState[form];
+  })
+})
+
+export default auth;

6220a6e8cff82dc42b0f781cb8154261eba2e893
Dongwoo-Kim <dongeu47@gmail.com>
2022-06-19 21:58:26 +0900

    220619

--- project/community/src/config/config.ts
+++ project/community/src/config/config.ts
@@ -2,6 +2,11 @@ const config = {
   url: {
     loginUrl: "http://localhost:5000/auth/login",
   },
+  db: {
+    host: "localhost",
+    username: "root",
+    password: "1234",
+  }
 };
 
 export default config;

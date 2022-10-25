1dbd838d715525a4b2decb814327b51643c1513c
Dongwoo-Kim <dongeu47@gmail.com>
2022-10-25 20:29:48 +0900

    221025
    221025

--- practice/image-upload-express/src/components/ImageForm.tsx
+++ practice/image-upload-express/src/components/ImageForm.tsx
@@ -1,18 +1,40 @@
+import axios from "axios";
 import { FormEvent, useState, ChangeEvent } from "react";
 
 const ImageForm = () => {
-  const [uploadFiles, setUploadFiles] = useState<File[] | never[]>([]);
-  const submitHanlder = (event: FormEvent<HTMLFormElement>) => {
+  const [uploadFiles, setUploadFiles] = useState<FileList | never[]>([]);
+
+  const submitHanlder = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    for (const file in uploadFiles) {
+      formData.append("file", file);
+    }
+
+    const res = await axios.post("http://localhost:5000/images", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    console.log(res);
   };
 
   const imageUploadHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const { files } = event.target;
+    const files = event.target.files as FileList;
+    setUploadFiles(files);
   };
 
   return (
-    <form encType="multipart/form-data" onSubmit={submitHanlder}>
-      <input type="file" name="image" onChange={imageUploadHandler} />
+    <form
+      action="http://localhost:5000/images"
+      encType="multipart/form-data"
+      onSubmit={submitHanlder}
+    >
+      <input type="file" name="image" onChange={imageUploadHandler} multiple />
+      <button type="submit">Send!</button>
     </form>
   );
 };
